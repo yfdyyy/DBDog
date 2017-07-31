fbc5699792442ef0fd4a9618c66b78ff8c864396
朱欢欢 <zhuhuanhuan@weidian.com>
2017-07-31 13:52:25 +0800

    使用cobar的语法解析

---
+++ engine/src/main/java/com/yfdyyy/App.java
@@ -0,0 +1,13 @@
+package com.yfdyyy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
